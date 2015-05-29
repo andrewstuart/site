18a2cdf765a826288258c9e2120d3c77024b3c17
Andrew Stuart <andrew.stuart2@gmail.com>
2015-05-29 16:47:30 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name astuartApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the astuartApp
+ */
+angular.module('astuartApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
